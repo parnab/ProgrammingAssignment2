93792ee73fa5035ad4087d23d550b78665e475de
parnab <parnab.2007@gmail.com>
2014-09-17 22:01:25 +0100

    parnab's second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## my comments
+## Given an Invertile matrix the following function calculates the inverse of 
+## the matrix and caches the value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat_inv <<- solve(x)
+  mat_inv
+    
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of an invertile matrix. The 
+## function takes advantage of already calculated cache value of the inverted 
+## matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(mat_inv)) {
+          message("getting cached data")
+          return(mat_inv)
+        }
+        mat_inv=makeCacheMatrix(x)
+        mat_inv
 }
